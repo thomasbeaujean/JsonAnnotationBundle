03ac328ebebb90751c956c9b197034f7b14556d8
thomasbeaujean <dev@vm-dev-tbe>
2013-11-27 23:00:05 +0100

    initial commit

---
+++ JsonAnnotationBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace thomasbeaujean\JsonAnnotationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use tbn\Bundle\JsonAnnotationBundle\DependencyInjection\Compiler\AddParamConverterPass;
+
+
+/**
+ * SensioFrameworkExtraBundle.
+ *
+ * @author     Thomas Beaujean
+ */
+class JsonAnnotationBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}
